337d722e606fb528e29214bf83e32528c464ac68
Arthur Padar <p-artu@yandex.ru>
2022-03-02 18:02:41 +0300

    sprint-2/step-2/attempt-finally

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
-import { compose, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { rootReducer } from './services/reducers/index';
-
-const composeEnhancers =
-  typeof window === 'object' && (window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']
-    ? (window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']({})
-    : compose;
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-const store = createStore(rootReducer, enhancer);
+import { store } from './services/store.js';
 
 ReactDOM.render(
   (<React.StrictMode>
